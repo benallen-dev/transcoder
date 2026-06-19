cdb9378568166d813b7bd0b4d416c78212d08a8d
Ben Allen <ben@benallen.dev>
2026-06-19 11:24:06 +0200

    factor out encoding logic

---
+++ errors.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+//  ── File not ready ───────────────────────────────────────────────────────
+type FileNotReadyError struct {
+	file    string
+	ogError error
+}
+
+func (e FileNotReadyError) Error() string {
+	return fmt.Sprintf("File not ready: %s. %s", e.file, e.ogError.Error())
+}
+
+func NewFileNotReadyError(f string) *FileNotReadyError {
+	return &FileNotReadyError{
+		file:    f,
+		ogError: errors.New("File is not ready"),
+	}
+}
+
+//  ── Command execution ────────────────────────────────────────────────────
+type CommandExecutionError struct {
+	file    string
+	command string
+	ogError error
+}
+
+func (e CommandExecutionError) Error() string {
+	return fmt.Sprintf("Error running command:\n\t%s\n\t%s\n\t%s", e.file, e.command, e.ogError.Error())
+}
+
+func NewCommandExecutionError(f string, c string, e error) *CommandExecutionError {
+	return &CommandExecutionError{
+		file:    f,
+		command: c,
+		ogError: e,
+	}
+}
--- main.go
+++ main.go
@@ -27,18 +27,78 @@ func isFileOpen(path string) (bool, error) {
 	return strings.TrimSpace(string(out)) != "", nil
 }
 
-func moveFileToProblemDir(conf *config.Config, file string) error {
-	ogLoc := filepath.Join(conf.Dirs.Watch, file)
-	newLoc := filepath.Join(conf.Dirs.Problem, file)
-	logger.Info("Moving input that saw an error to problem dir", "file", file)
+func moveFileToProblemDir(conf *config.Config, f string) error {
+	ogLoc := filepath.Join(conf.Dirs.Watch, f)
+	newLoc := filepath.Join(conf.Dirs.Problem, f)
+	logger.Info("Moving input that saw an error to problem dir", "file", f)
 	return os.Rename(ogLoc, newLoc)
 }
 
-func moveFileToDoneDir(conf *config.Config, file string) error {
-	ogLoc := filepath.Join(conf.Dirs.Watch, file)
-	newLoc := filepath.Join(conf.Dirs.Done, file)
-	logger.Info("Moving processed file to done", "file", file)
+func moveFileToDoneDir(conf *config.Config, f string) error {
+	ogLoc := filepath.Join(conf.Dirs.Watch, f)
+	newLoc := filepath.Join(conf.Dirs.Done, f)
+	logger.Info("Moving processed file to done", "file", f)
 	return os.Rename(ogLoc, newLoc)
+}
+
+func encodeFile(conf *config.Config, f string) error {
+	loc := filepath.Join(conf.Dirs.Watch, f)
+
+	// Check if the file is open in another process
+	isOpen, err := isFileOpen(loc)
+	if err != nil {
+		return err
+	}
+	if isOpen {
+		return NewFileNotReadyError(loc)
+	}
+
+	// Build the ffmpeg command
+	var width = strconv.Itoa(conf.Output.MaxWidth)
+	var height = strconv.Itoa(conf.Output.MaxHeight)
+	var bitrate = strconv.Itoa(conf.Output.MaxBitrate)
+	var bufsize = strconv.Itoa(2 * conf.Output.MaxBitrate)
+	var crf = strconv.Itoa(conf.Output.Crf)
+	var targetFile = filepath.Join(conf.Dirs.Output, f+".mp4")
+
+	cmd := exec.Command("ffmpeg",
+		"-i", loc,
+		"-vf", fmt.Sprintf("scale='min(%s,iw)':'min(%s,ih)':force_original_aspect_ratio=decrease,scale=trunc(iw/2)*2:trunc(ih/2)*2", width, height),
+
+		"-b:v", bitrate+"M",
+		"-maxrate", bitrate+"M",
+		"-bufsize", bufsize+"M",
+		"-map", "0:v:0",
+		"-map", "0:m:language:eng",
+		"-map", "0:a:0",
+		"-map", "-0:s", // strip subs
+		"-ac", "2",
+
+		"-c:v", "libx265",
+		"-tag:v", "hvc1",
+		"-crf", crf,
+		"-x265-params", "keyint=60:min-keyint=60:no-scenecut=1",
+		"-movflags", "+faststart",
+
+		"-preset", "fast",
+		"-c:a", "aac",
+		"-b:a", "192k",
+		targetFile,
+	)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		logger.Error("Error running encoding command, removing output file, ", "file", targetFile)
+		os.Remove(targetFile)
+		return NewCommandExecutionError(f, cmd.String(), err)
+	}
+
+	moveFileToDoneDir(conf, f)
+
+	return nil
 }
 
 func main() {
@@ -64,69 +124,19 @@ func main() {
 
 		// Assemble the input filename
 		f := files[0].Name()
-		loc := filepath.Join(conf.Dirs.Watch, f)
-
-		// Check if the file is open in another process
-		isOpen, err := isFileOpen(loc)
+		err = encodeFile(conf, f)
 		if err != nil {
-			logger.Error("Error checking if file is open", "file", f, "error", err)
-			moveFileToProblemDir(conf, f)
-			continue
-		}
-
-		if isOpen {
-			logger.Info("File is not ready, waiting", "file", f)
-			time.Sleep(conf.NapTime)
-			continue
-		}
-
-		// Build the ffmpeg command
-		var width = strconv.Itoa(conf.Output.MaxWidth)
-		var height = strconv.Itoa(conf.Output.MaxHeight)
-		var bitrate = strconv.Itoa(conf.Output.MaxBitrate)
-		var bufsize = strconv.Itoa(2 * conf.Output.MaxBitrate)
-		var crf = strconv.Itoa(conf.Output.Crf)
-		var targetFile = filepath.Join(conf.Dirs.Output, f+".mp4")
-
-		cmd := exec.Command("ffmpeg",
-			"-i", loc,
-			"-vf", fmt.Sprintf("scale='min(%s,iw)':'min(%s,ih)':force_original_aspect_ratio=decrease,scale=trunc(iw/2)*2:trunc(ih/2)*2", width, height),
-
-			"-b:v", bitrate+"M",
-			"-maxrate", bitrate+"M",
-			"-bufsize", bufsize+"M",
-			"-map", "0:v:0",
-			"-map", "0:m:language:eng",
-			"-map", "0:a:0",
-			"-map", "-0:s", // strip subs
-			"-ac", "2",
-
-			"-c:v", "libx265",
-			"-tag:v", "hvc1",
-			"-crf", crf,
-			"-x265-params", "keyint=60:min-keyint=60:no-scenecut=1",
-			"-movflags", "+faststart",
-
-			"-preset", "fast",
-			"-c:a", "aac",
-			"-b:a", "192k",
-			targetFile,
-		)
-
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		err = cmd.Run()
-		if err != nil {
-			logger.Error("Failed to run command")
-			logger.Error(cmd.String())
-			logger.Error(err.Error())
-
-			moveFileToProblemDir(conf, f)
-
-			logger.Error("Removing output file", "file", targetFile)
-			os.Remove(targetFile)
-			continue
+			switch err.(type) {
+			case FileNotReadyError:
+				// Place it at the back of the queue once we have
+				// one, but for now just wait and try again
+				time.Sleep(conf.NapTime)
+				continue
+			default:
+				logger.Error(err.Error())
+				moveFileToProblemDir(conf, f)
+				continue
+			}
 		}
 
 		moveFileToDoneDir(conf, f)
